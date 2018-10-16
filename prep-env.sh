f41f2b822605971503913724fbb80e52639f6b50
Truman Zhou <truman.ck.zhou@gmail.com>
2018-10-16 13:14:52 +0800

    fix ci

--- prep-env.sh
+++ prep-env.sh
@@ -21,5 +21,11 @@ gtest(){
 }
 
 cd $BASE_DIR
-gtest
+#gtest
 
+cd /usr/src/gtest
+sudo cmake CMakeLists.txt
+sudo make
+ 
+# copy or symlink libgtest.a and libgtest_main.a to your /usr/lib folder
+sudo cp *.a /usr/lib

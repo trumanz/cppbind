de98a084ed59cc5ff955c5e1b2c0b4e5116b339c
Truman Zhou <truman.ck.zhou@gmail.com>
2019-12-23 20:58:36 +0800

    add sonar

---
+++ sonar-check.sh
@@ -0,0 +1,21 @@
+﻿#!/usr/bin/env bash
+
+set -e 
+
+
+export SONAR_SCANNER_VERSION=4.2.0.1873
+export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
+rm -rf $SONAR_SCANNER_HOME
+mkdir -p $SONAR_SCANNER_HOME
+curl -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
+unzip $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
+rm $HOME/.sonar/sonar-scanner.zip
+export PATH=$SONAR_SCANNER_HOME/bin:$PATH
+export SONAR_SCANNER_OPTS="-server"
+
+sonar-scanner \
+  -Dsonar.projectKey=trumanz_cppbind \
+  -Dsonar.organization=trumanz \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=ed79d3f3b5286851a2265f9b060d466dbba121d0

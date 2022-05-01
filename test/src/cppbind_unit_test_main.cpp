50d391b520f2b396f515683b69f337f9a67bd625
Truman Zhou <changkai.zhou@outlook.com>
2022-05-01 16:49:23 +0800

    chg: rollback to c++ 14

--- test/src/cppbind_unit_test_main.cpp
+++ test/src/cppbind_unit_test_main.cpp
@@ -1,9 +1,9 @@
 ﻿#include <stdio.h>
 #include "gtest/gtest.h"
-#include <filesystem>
+//#include <filesystem>
 
 int main(int argc, char *argv[]){
-    std::cout << "Current Path:" << std::filesystem::current_path()  << std::endl;
+    //std::cout << "Current Path:" << std::filesystem::current_path()  << std::endl;
 
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
